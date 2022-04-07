2ca3bf7928ec53193d2a794e2df8cd58fce07679
emilyhi45427 <emilyhi45427@tamu.edu>
2022-04-07 12:19:37 -0500

    Adjusted map position and scaling

--- Main/static/additional.js
+++ Main/static/additional.js
@@ -1,9 +1,10 @@
 // Initialize and add map display
 function initMap() {
     // College Station coords
-    const cstatcoords = {lat: 30.626031, lng: -96.335160};
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
+    var cstatcoords = {lat: 30.626031, lng: -96.335160};
+    var mapOptions = {
         center: cstatcoords,
-    });
+        zoom: 14
+    };
+    const map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
